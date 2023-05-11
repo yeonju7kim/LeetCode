b54094f58b1b6f4200da227a8f0cc6b4cb789d88
yeonju7kim <95571735+yeonju7kim@users.noreply.github.com>
2023-05-11 13:52:27 +0900

    Time: 190 ms (70.87%), Space: 16.7 MB (33.86%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -4,12 +4,12 @@ class Solution:
         n2 = len(nums2)
         
         dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
-
-        for i in range(1, n1 + 1):
-            for j in range(1, n2 + 1):
-                if nums1[i - 1] == nums2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+        
+        for i in range(n1):
+            for j in range(n2):
+                if nums1[i] == nums2[j]:
+                    dp[i + 1][j + 1] = dp[i][j] + 1  
                 else:
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
-
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
         return dp[n1][n2]
+                
